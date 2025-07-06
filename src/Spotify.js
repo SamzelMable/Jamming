903c517b3c576af2806c82198df4226b726bb2e4
SamzelMable <samzelmable@gmail.com>
2025-07-06 10:49:03 +1000

    Fix Spotify authorization URL

--- src/Spotify.js
+++ src/Spotify.js
@@ -3,25 +3,22 @@ const redirectUri = 'https://SamzelMable.github.io/Jammming/';
 let accessToken;
 
 const Spotify = {
-  getAccessToken() {
-    if (accessToken) return accessToken;
+ getAccessToken() {
+  if (accessToken) return accessToken;
 
-    const tokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+  const tokenMatch = window.location.href.match(/access_token=([^&]*)/);
+  const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
-    if (tokenMatch && expiresInMatch) {
-      accessToken = tokenMatch[1];
-      const expiresIn = Number(expiresInMatch[1]);
-
-      // Clear the token after it expires
-      window.setTimeout(() => accessToken = '', expiresIn * 1000);
-      window.history.pushState('Access Token', null, '/'); // Clean the URL
-      return accessToken;
-    } else {
-      const accessUrl =
-        `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${encodeURIComponent(redirectUri)}`;
-      window.location = accessUrl;
-    }
+  if (tokenMatch && expiresInMatch) {
+    accessToken = tokenMatch[1];
+    const expiresIn = Number(expiresInMatch[1]);
+    window.setTimeout(() => accessToken = '', expiresIn * 1000);
+    window.history.pushState('Access Token', null, '/');
+    return accessToken;
+  } else {
+    const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${encodeURIComponent(redirectUri)}`;
+    window.location = accessUrl;
+  }
   },
 
   async search(term) {
