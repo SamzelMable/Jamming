dcb8416047b77eec84777268b8f4773e6e877a88
SamzelMable <samzelmable@gmail.com>
2025-07-07 20:43:03 +1000

    added exprt to Spotify.js

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,47 +1,46 @@
+// src/Spotify.js
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
+
 const clientId = '11cee0b5382a406f930c84c51920adc4';
 const redirectUri = 'https://samzelmable.github.io/Jamming/';
-let accessToken;
+const scopes = ['playlist-modify-public'];
 
 const Spotify = {
   getAccessToken() {
-    if (accessToken) return accessToken;
+    const params = new URLSearchParams(window.location.hash);
+    const token = params.get('access_token');
 
-    const tokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-
-    if (tokenMatch && expiresInMatch) {
-      accessToken = tokenMatch[1];
-      const expiresIn = Number(expiresInMatch[1]);
-      window.setTimeout(() => accessToken = '', expiresIn * 1000);
-      window.history.pushState('Access Token', null, '/');
-      return accessToken;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+      return token;
     } else {
-      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${encodeURIComponent(redirectUri)}`;
-      window.location = accessUrl;
+      const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes.join(' '))}`;
+      window.location.href = authUrl;
     }
   },
 
   async search(term) {
-    const token = Spotify.getAccessToken();
-    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (!jsonResponse.tracks) return [];
-        return jsonResponse.tracks.items.map(track => ({
-          id: track.id,
-          name: track.name,
-          artist: track.artists[0].name,
-          album: track.album.name,
-          uri: track.uri
-        }));
-      });
-  }
+    const token = this.getAccessToken();
+    if (!token) return [];
 
-  // Add savePlaylist method later
+    try {
+      const response = await spotifyApi.searchTracks(term);
+      return response.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }));
+    } catch (error) {
+      console.error('Spotify search error:', error);
+      return [];
+    }
+  },
+
+  // Future: Save playlist etc.
 };
 
 export default Spotify;
